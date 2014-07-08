eb1d54a9e473c18981e6ac9ea864714cef98973b
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-08 15:38:36 -0400

    poll new, create, show

--- pickleback/app/controllers/polls_controller.rb
+++ pickleback/app/controllers/polls_controller.rb
@@ -1,9 +1,21 @@
 class PollsController < ApplicationController
+  before_action :set_poll, only: [:show, :edit, :update, :destroy]
+
   def new
     @poll = Poll.new
+    2.times {@poll.options.new}
+  end
+
+  def show
   end
 
   def create
+    @poll = Poll.new(poll_params)
+    if @poll.save
+      redirect_to @poll
+    else
+      render "new", notice: "Please enter a question with two answers."
+    end
   end
 
   def edit
@@ -13,5 +25,14 @@ class PollsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def set_poll
+    @poll = Poll.find(params[:id])
+  end
+
+  def poll_params
+    params.require(:poll).permit(:question, :user_id, :options_attributes => [:answer])
   end
 end
