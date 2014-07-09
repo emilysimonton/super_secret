1ea349797d08f5b7e04f2ed8c9a3586d0d0b6b8e
ericodes <erica.cheung@flatironschool.com>
2014-07-09 10:59:02 -0400

    poll timestamps appearing and saving

--- pickleback/config/application.rb
+++ pickleback/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Pickleback
   class Application < Rails::Application
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
