b5212ff5c328b17a84482351b2cfee5cdb42e8db
emilysimonton <emily.simonton@gmail.com>
2014-07-09 15:46:34 -0400

    added recipient

--- pickleback/app/controllers/recipients_controller.rb
+++ pickleback/app/controllers/recipients_controller.rb
@@ -1,15 +1,28 @@
 class RecipientsController < ApplicationController
   def new
+    # binding.pry
     # GET: asks you to share or input links
-    3.times {@recipient.new}
+    @poll = Poll.find(params[:poll_id])
+    3.times {@poll.recipients.new}
   end
 
   def create
+    @poll = Poll.find(params[:poll_id])
+    params[:recipients].each do |recipient| 
+      Recipient.create(:email => recipient[:email], :poll_id => @poll.id)
+    end
+      # if @recipient.save
+      # UserMailer.registration_confirmation(@user).deliver
     # POST: adds recipients to database
     # calls method to send emails or create link
-    
+
   end
 
   def destroy
+  end
+
+  private
+  def poll_params
+    params.require(:recipient).permit(:email, :poll_id)
   end
 end
