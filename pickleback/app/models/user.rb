3b0026a55f90f8b14bf53fa861928a5018fa3ee9
Sam Blake <samuelmorganblake@gmail.com>
2014-07-10 11:57:31 -0400

    devise migration

--- pickleback/app/models/user.rb
+++ pickleback/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :polls
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
