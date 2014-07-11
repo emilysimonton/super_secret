5df743b7ebe87257d9e88378f1abcc6625734370
Sam Blake <samuelmorganblake@gmail.com>
2014-07-11 12:00:57 -0400

    closing poll after expiration date

--- pickleback/app/controllers/voters_controller.rb
+++ pickleback/app/controllers/voters_controller.rb
@@ -2,25 +2,35 @@ class VotersController < ApplicationController
 
   def new
     @poll = Poll.find_by(token: params[:token])
-    @poll.voters.each do |voter|
-      if session[:voter] == voter.uuid
-        flash[:notice] = "You've already voted on this poll!"
-        redirect_to root_path
-      end 
-    end 
+    if Time.now <= @poll.expiration
+      @poll.voters.each do |voter|
+        if session[:voter] == voter.uuid
+          flash[:notice] = "You've already voted on this poll!"
+          redirect_to root_path
+        else
+          render :new
+        end
+      end
+    else
+        redirect_to result_path(@poll.id)
+    end
   end
 
-  def create
-    @poll = Poll.find_by(token: params[:token])
-    Option.increment_counter(:votes, params[:option])
-    session[:voter] ||= SecureRandom.uuid
-    voter = Voter.create(uuid: session[:voter])
-    voter.polls << @poll
-    redirect_to vote_path(@poll.token)
-  end
 
-  def show
-  end
+
+
+    def create
+      @poll = Poll.find_by(token: params[:token])
+      Option.increment_counter(:votes, params[:option])
+      session[:voter] ||= SecureRandom.uuid
+      voter = Voter.create(uuid: session[:voter])
+      voter.polls << @poll
+      redirect_to vote_path(@poll.token)
+    end
+
+  # def show
+  #   @poll = Poll.find_by(token: params[:token])
+  # end
 
 
 end
