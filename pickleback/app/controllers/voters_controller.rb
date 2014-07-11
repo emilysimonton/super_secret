7b627792464ace91029cb1de728b009892b7aee0
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-11 12:21:55 -0400

    resolve session clearing issue

--- pickleback/app/controllers/voters_controller.rb
+++ pickleback/app/controllers/voters_controller.rb
@@ -14,7 +14,7 @@ class VotersController < ApplicationController
     @poll = Poll.find_by(token: params[:token])
     Option.increment_counter(:votes, params[:option])
     session[:voter] ||= SecureRandom.uuid
-    voter = Voter.create(uuid: session[:voter])
+    voter = Voter.find_or_create_by(uuid: session[:voter])
     voter.polls << @poll
     redirect_to vote_path(@poll.token)
   end
