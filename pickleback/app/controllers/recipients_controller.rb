219580cb1ea87bfe4ad16590988f57e29ecc8e24
ericodes <erica.cheung@flatironschool.com>
2014-07-14 11:13:40 -0400

    mailer works!

--- pickleback/app/controllers/recipients_controller.rb
+++ pickleback/app/controllers/recipients_controller.rb
@@ -6,10 +6,11 @@ class RecipientsController < ApplicationController
 
   def create
     @poll = Poll.find(params[:poll_id])
-    params[:recipients].each do |recipient|
-      #UserMailer.registration_confirmation(recipient).deliver
+    session[:recipients] = []
+    session[:recipients] = params[:recipients]
+    session[:recipients].each do |recipient|
+      UserMailer.poll_sender(@poll, recipient).deliver
     end
-
   end
 
   def destroy
--- pickleback/app/mailers/user_mailer.rb
+++ pickleback/app/mailers/user_mailer.rb
@@ -1,10 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "flatiron.picklebacks@gmail.com"
-  def poll_sender(@poll)
-    @user = User.find(@poll.user_id)
-    # After we create a user table!
-    @poll.recipients.each do |recipient|
-      mail(:to => recipient.email, :subject => "#{@user.name} has a question for you")
-    end
+  def poll_sender(poll, recipient)
+    @poll = poll
+    @recipient = recipient
+    @user = User.find(poll.user_id)
+    mail(:to => recipient, :subject => "#{@user.name} has a question for you")
   end
 end
