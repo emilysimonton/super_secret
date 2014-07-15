2ac9797473739c380f068c93f4c79cd6ede3d060
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-15 17:39:25 -0400

    format countdown clock better

--- pickleback/app/assets/javascripts/polls.js
+++ pickleback/app/assets/javascripts/polls.js
@@ -1,4 +1,4 @@
 $( document ).on( 'page:change', function () {
   var expiration = $('#countdownClock').data('expiration');
   $('#countdownClock').countdown({until: new Date(expiration)});
-})
+});
