b69866372319241168348ddba782789da560e3f4
Sam Blake <samuelmorganblake@gmail.com>
2014-07-16 14:19:21 -0400

    added google authentication

--- pickleback/db/schema.rb
+++ pickleback/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716154413) do
+ActiveRecord::Schema.define(version: 20140716170341) do
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20140716154413) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
