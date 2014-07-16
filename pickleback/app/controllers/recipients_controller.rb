2c33a27b13d4f9f65c41d6a261e9bc577c714c34
ericodes <erica.cheung@flatironschool.com>
2014-07-16 15:29:18 -0400

    Made recipients email field a single field

--- pickleback/app/controllers/recipients_controller.rb
+++ pickleback/app/controllers/recipients_controller.rb
@@ -1,23 +1,43 @@
 class RecipientsController < ApplicationController
+  before_action :set_poll, only: [:index, :new, :create]
+  def index
+    @recipients = session[:recipients]
+  end
+
   def new
-    # GET: asks you to share or input links
-    @poll = Poll.find(params[:poll_id])
   end
 
   def create
-    @poll = Poll.find(params[:poll_id])
-    @message = params[:message]
-    @recipients = []
-    @recipients = params[:recipients].reject(&:empty?)
-    @recipients.each do |recipient|
-      UserMailer.poll_sender(@poll, recipient, @message).deliver
+    @message = params[:message]    
+    # Approach 2: For single text box, comma separated emails
+    session[:recipients] = params[:recipients]
+    @recipients = session[:recipients]
+    @invalid_emails = []
+    @recipients.split(",").each do |email|
+      email = email.strip
+      email_errors = ValidatesEmailFormatOf::validate_email_format(email)
+      @invalid_emails.push(email) if (email_errors && !email.empty?)
     end
-  end
+    if @invalid_emails.empty?
+      redirect_to poll_recipients_path(@poll)
+    else
+      flash.now[:error] = "The following emails are invalid: #{@invalid_emails.join(', ')}"
+      render :new
+    end
 
-  def destroy
+    # Approach 1: For multiple input boxes for emails
+    # @recipients = []
+    # @recipients = params[:recipients].reject(&:empty?)
+    # @recipients.each do |recipient|
+    #   UserMailer.poll_sender(@poll, recipient, @message).deliver
+    # end
   end
 
   private
+  def set_poll
+    @poll = Poll.find(params[:poll_id])
+  end
+
   def poll_params
     params.require(:recipient).permit(:email, :poll_id)
   end
