2a356959dc32bf198d73e1f1940e0f06e2af1993
ericodes <erica.cheung@flatironschool.com>
2014-07-16 15:39:24 -0400

    added error alert if no email recipients entered

--- pickleback/app/controllers/recipients_controller.rb
+++ pickleback/app/controllers/recipients_controller.rb
@@ -9,28 +9,25 @@ class RecipientsController < ApplicationController
 
   def create
     @message = params[:message]    
-    # Approach 2: For single text box, comma separated emails
     session[:recipients] = params[:recipients]
     @recipients = session[:recipients]
     @invalid_emails = []
+
     @recipients.split(",").each do |email|
       email = email.strip
       email_errors = ValidatesEmailFormatOf::validate_email_format(email)
       @invalid_emails.push(email) if (email_errors && !email.empty?)
     end
-    if @invalid_emails.empty?
+    
+    if @recipients.empty?
+      flash.now[:error] = "No emails entered"
+      render :new
+    elsif @invalid_emails.empty?
       redirect_to poll_recipients_path(@poll)
     else
       flash.now[:error] = "The following emails are invalid: #{@invalid_emails.join(', ')}"
       render :new
     end
-
-    # Approach 1: For multiple input boxes for emails
-    # @recipients = []
-    # @recipients = params[:recipients].reject(&:empty?)
-    # @recipients.each do |recipient|
-    #   UserMailer.poll_sender(@poll, recipient, @message).deliver
-    # end
   end
 
   private
