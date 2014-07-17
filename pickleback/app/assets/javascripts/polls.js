3e52d4a789185027db3fd65f062586b4573af109
Janel diBiccari <janel.dibiccari@gmail.com>
2014-07-17 14:33:10 -0400

    change countdown clock

--- pickleback/app/assets/javascripts/polls.js
+++ pickleback/app/assets/javascripts/polls.js
@@ -1,4 +1,9 @@
 $( document ).on( 'page:change', function () {
-  var expiration = $('#countdownClock').data('expiration');
-  $('#countdownClock').countdown({until: new Date(expiration)});
+  var expiration = $('.countdown').data('expiration');
+  $('.countdown').countdown({
+    date: new Date(expiration),
+    render: function(data) {
+      $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    } 
+  });
 });
