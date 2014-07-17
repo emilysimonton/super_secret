b5148b45ca3252e42020bbb319eb2fcb93aa1595
ericodes <erica.cheung@flatironschool.com>
2014-07-17 13:49:04 -0400

    poll slides are appearing!

--- pickleback/app/assets/javascripts/application.js
+++ pickleback/app/assets/javascripts/application.js
@@ -43,12 +43,12 @@ $(function() {
     });
 });
 
-// $(document).ready(function() {
-//   $('#slides').superslides({
-//     slide_easing: 'easeInOutCubic',
-//     slide_speed: 800,
-//     pagination: true,
-//     hashchange: true,
-//     scrollable: true
-//   });
-// });
+$(document).ready(function() {
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    pagination: true,
+    hashchange: true,
+    scrollable: true
+  });
+});
